d29b0e4c82f4de02018c1981dd5ff7efaee2e258
jhonsbaracaldo <jhonsbaracaldo@gmail.com>
2023-11-24 16:56:03 -0500

    implementando interfaces

--- src/main/java/org/example/Services/GestorAcademico.java
+++ src/main/java/org/example/Services/GestorAcademico.java
@@ -7,10 +7,30 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class GestorAcademico {
+public class GestorAcademico implements ServiciosAcademicosI {
 
    private List<Estudiante> Estudiantes;
 
     private List<Curso> cursos;
     HashMap<String, ArrayList<String>> Estudianteycursos = new HashMap<>();
+
+    @Override
+    public void matricularEstudiante(Estudiante estudiante) {
+
+    }
+
+    @Override
+    public void agregarCurso(Curso curso) {
+
+    }
+
+    @Override
+    public void inscribirEstudianteCurso(Estudiante estudiante, int idCurso) {
+
+    }
+
+    @Override
+    public void desinscribirEstudianteCurso(int idEstudiante, int idCurso) {
+
+    }
 }
